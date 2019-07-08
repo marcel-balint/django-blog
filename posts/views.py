7a913e70c6eb4f58ffcf44b6b06bad7caf836fce
Ubuntu <ubuntu@ip-172-31-85-98.ec2.internal>
2019-07-08 13:05:41 +0000

    Created the views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import BlogPostForm
 
-# Create your views here.
+
+def get_posts(requests):
+    """
+    Create a view that will return a list
+    
